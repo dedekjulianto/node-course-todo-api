086162a737dadcc1e52677c5382cfccb0c8e342c
dedekjulianto <dedekproject@gmail.com>
2018-09-14 12:54:23 +0700

    Add update script

---
+++ playground/mongodb-update.js
@@ -0,0 +1,39 @@
+const {MongoClient, ObjectID} = require('mongodb');
+
+const url = 'mongodb://localhost:27017/TodoApp';
+
+MongoClient.connect(url, (err, db) => {
+	if (err) {
+		return console.log('Unable to connect to MongoDB server');
+	}
+	console.log('Connected to MongoDB server');
+
+	// db.collection('Todos').findOneAndUpdate({
+	// 	_id: new ObjectID('5b9ab70143271a22e7e5daa8')
+	// }, {
+	// 	$set: {
+	// 		completed: true
+	// 	}
+	// }, {
+	// 	returnOriginal: false
+	// }).then((result) => {
+	// 	console.log(result);
+	// });
+
+	db.collection('Users').findOneAndUpdate({
+		_id: new ObjectID('5b9ace758a450d3bec133a36')
+	}, {
+		$set: {
+			name: 'Dedek Julianto'
+		},
+		$inc: {
+			age: 1
+		}
+	}, {
+		returnOriginal: false
+	}).then((result) => {
+		console.log(result);
+	});
+
+	// db.close();
+});
