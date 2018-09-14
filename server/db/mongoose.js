77fa77de031a3231e09bc4fb95a1f1104a223f36
dedekjulianto <dedekproject@gmail.com>
2018-09-15 01:21:20 +0700

    Test POST /todos route

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,8 @@
 var mongoose = require('mongoose');
+// mongoose.set('useFindAndModify', false);
+// mongoose.set('useCreateIndex', true);
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://localhost:27017/TodoApp', { useNewUrlParser: true });
 
 module.exports = { mongoose };
