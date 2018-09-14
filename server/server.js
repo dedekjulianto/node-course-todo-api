369a4dc46b8f601103768d42edfe74a4a341a87e
dedekjulianto <dedekproject@gmail.com>
2018-09-15 00:14:11 +0700

    Add test cases for GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -40,7 +40,7 @@ app.get('/todos/:id', (req, res) => {
 	}
 	Todo.findById(id).then((todo) => { // findById
 		if (!todo) { // if no todo -
-			return res.status.send(); // - send back 404 with empty body
+			return res.status(404).send(); // - send back 404 with empty body
 		}
 		res.send({todo});
 	}).catch((e) => {
