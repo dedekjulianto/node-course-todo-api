73c7f29e3903ca130af965214490e990d24bcbf2
dedekjulianto <dedekproject@gmail.com>
2018-09-15 17:13:55 +0700

    Setup seperate test and development envs

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,6 +3,6 @@ var mongoose = require('mongoose');
 // mongoose.set('useCreateIndex', true);
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp', { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
 
 module.exports = { mongoose };
