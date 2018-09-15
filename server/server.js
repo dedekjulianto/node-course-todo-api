ab1083533a5f9670bbc18306cce43605b13ca1e4
dedekjulianto <dedekproject@gmail.com>
2018-09-15 14:07:08 +0700

    Add DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -48,6 +48,24 @@ app.get('/todos/:id', (req, res) => {
 	});
 });
 
+app.delete('/todos/:id', (req, res) => {
+	// get the id
+	var id = req.params.id;
+
+	// validate the id -> not valid? return 404
+	if(!ObjectID.isValid(id)) {
+		return res.status(404).send();
+	}
+	Todo.findByIdAndRemove(id).then((doc) => {
+		if(!doc) {
+			return res.status(404).send();
+		}
+		res.send(doc);
+	}).catch((error) => {
+		res.status(400).send(); // 400 - with empty body
+	});
+});
+
 app.listen(port, () => {
 	console.log(`Started up at port ${port}`);
 });
